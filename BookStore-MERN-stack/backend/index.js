8086f8b10262e5cfbc11c341a1197e886b77f957
Sumanjeet <Sumanjeet0012@gmail.com>
2023-09-19 00:24:21 +0530

    A simple route has benn created

--- BookStore-MERN-stack/backend/index.js
+++ BookStore-MERN-stack/backend/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import { PORT } from "./config.js";
 const app = express();
 
+app.get("/", (request,response)=> {
+    console.log(request)
+    return response.status(234).send('Welcome to MERN stack project')
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening at port: ${PORT}`);
 });
