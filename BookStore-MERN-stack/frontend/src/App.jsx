4bf78eca395204d65611ad714b7f5fb43e24893f
Sumanjeet <Sumanjeet0012@gmail.com>
2023-09-20 00:18:41 +0530

    added react router dom

--- BookStore-MERN-stack/frontend/src/App.jsx
+++ BookStore-MERN-stack/frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element= {<Home />} />
+      <Route path='/books/create' element= {<CreateBook />} />
+      <Route path='/books/details/:id' element= {<ShowBook />} />
+      <Route path='/books/edit/:id' element= {<EditBook />} />
+      <Route path='/books/delete/:id' element= {<DeleteBooks />} />
+
+    </Routes>
   )
 }
 
