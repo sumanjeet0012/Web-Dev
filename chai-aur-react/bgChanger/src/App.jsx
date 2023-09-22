36f04a2fc5bf941afebbf3e5e1c31503285104c0
Sumanjeet <Sumanjeet0012@gmail.com>
2023-09-22 20:19:39 +0530

    password generator is created

--- chai-aur-react/bgChanger/src/App.jsx
+++ chai-aur-react/bgChanger/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react"
 
-
-
 function App() {
   const [color, setColor] = useState('olive')
 
---
+++ chai-aur-react/test/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
+}
+
+export default App
